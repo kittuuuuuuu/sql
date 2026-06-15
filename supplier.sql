ae827b195e3a37ba952e9814207548db122f47aa
kittuuuuuuu <kritikaarora.stats@gmail.com>
2026-06-15 16:13:46 +0530

    Update supplier.sql

---
+++ supplier.sql
@@ -0,0 +1,8 @@
+create table supplier(
+snumber text primary KEy,
+sname text,
+sage int);
+INSERT INTO supplier(snumber,sname,sage)values
+("1","shubham",14),
+("2","kritika",15);
+select * from supplier;
