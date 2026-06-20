f539ba5a12ef6c55e52efc627da0a927bbbedd68
kittuuuuuuu <kritikaarora.stats@gmail.com>
2026-06-20 19:13:21 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,11 @@
+create table student(
+roll_No int primary key,
+name text,
+adress text,
+age int);
+Insert into student(roll_no,name,adress,age) VALUES
+(1,"ram","delhi",12),
+(2,"seeta","delhi",12),
+(3,"kritika","gurgaon",14),
+(4,"sham","karnal",16);
+select * from student
